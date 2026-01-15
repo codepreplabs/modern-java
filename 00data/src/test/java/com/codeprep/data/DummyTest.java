544668df3fa9e848763461e86a8743f090e94fc3
Praveen <thejavamonk@gmail.com>
2026-01-15 12:37:21 +0530

    Data for examples

---
+++ 00data/src/test/java/com/codeprep/data/DummyTest.java
@@ -0,0 +1,15 @@
+package com.codeprep.data;
+
+/**
+ * Dummy test class for demonstration purposes
+ * This is a placeholder test file
+ */
+public class DummyTest {
+
+    public void testDummyMethod() {
+        String expected = "Hello, Data!";
+        String actual = "Hello, Data!";
+        System.out.println("Testing dummy method");
+    }
+}
+
