56ba16d574e8d140de966d9effe4e9525d40cb41
Praveen <thejavamonk@gmail.com>
2026-01-18 18:53:19 +0530

    examples on Date and Time API changes in Java 8

---
+++ 08date-time/src/test/java/com/codeprep/datetime/DummyTest.java
@@ -0,0 +1,14 @@
+package com.codeprep.datetime;
+
+/**
+ * Dummy test class for demonstration purposes
+ * This is a placeholder test file
+ */
+public class DummyTest {
+
+    public void testDummyMethod() {
+        String expected = "Hello, Date Time API!";
+        String actual = "Hello, Date Time API!";
+        System.out.println("Testing dummy method");
+    }
+}
