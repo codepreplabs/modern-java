6aabaad3320fa19aaf683a9b81eb2465d4f297e4
Praveen <thejavamonk@gmail.com>
2026-01-18 18:53:01 +0530

    examples on use of default and static methods in interfaces

---
+++ 07default-static-methods/src/test/java/com/codeprep/defaultstaticmethods/DummyTest.java
@@ -0,0 +1,14 @@
+package com.codeprep.defaultstaticmethods;
+
+/**
+ * Dummy test class for demonstration purposes
+ * This is a placeholder test file
+ */
+public class DummyTest {
+
+    public void testDummyMethod() {
+        String expected = "Hello, Default and Static Methods!";
+        String actual = "Hello, Default and Static Methods!";
+        System.out.println("Testing dummy method");
+    }
+}
